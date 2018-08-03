1a0352f776da1b9bc24a36e1c0e76cff03fa944a
Troels Bay-Petersen <troelsbaypetersen@hotmail.com>
2018-08-03 17:21:01 +0200

    Initial commit.

---
+++ PaintWithFriends/PaintHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaintWithFriends
+{
+    public class PaintHub : Hub
+    {
+        /*public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }*/
+
+        public async Task MoveMouse(int x, int y, bool isPainting)
+        {
+            await Clients.All.SendAsync("MoveMouse", x, y, isPainting);
+        }
+
+        public async Task MouseDown()
+        {
+            await Clients.All.SendAsync("MouseDown");
+        }
+
+        public async Task MouseUp()
+        {
+            await Clients.All.SendAsync("MouseUp");
+        }
+
+        public async Task ChangeColor(string color)
+        {
+            await Clients.All.SendAsync("ChangeColor", color);
+        }
+
+        public async Task ChangeBrushSize(int size)
+        {
+            await Clients.All.SendAsync("ChangeBrushSize", size);
+        }
+
+        public async Task ChangeMode(string mode)
+        {
+            await Clients.All.SendAsync("ChangeMode", mode);
+        }
+    }
+}
